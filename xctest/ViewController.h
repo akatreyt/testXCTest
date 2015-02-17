91941691fd793cb3bc3de4daeaafa403d4a65dd4
Gary T <gary.tartt@pepsico.com>
2015-02-17 10:41:58 -0600

    Initial Commit

---
+++ xctest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xctest
+//
+//  Created by Tartt, Gary {PBSG} on 2/17/15.
+//  Copyright (c) 2015 Tartt, Gary {PBSG}. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xctest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xctest
+//
+//  Created by Tartt, Gary {PBSG} on 2/17/15.
+//  Copyright (c) 2015 Tartt, Gary {PBSG}. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
